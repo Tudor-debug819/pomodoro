1f0689d070f0cd09451aff374432cef154220e51
Tudor-debug819 <tovidiua@emeal.nttdata.com>
2025-05-07 01:00:34 +0300

    fdfsd

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { timerReducer } from './app/timer/timer.reducer';
 import { provideState, provideStore } from '@ngrx/store';
-import { isDevMode } from '@angular/core';
-import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideState('timer', timerReducer),
-    provideStore(), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          }), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          }),
+    provideStore(), 
   ]
 
 });
